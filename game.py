7b3007515a00a052008ddf3032d7fd0714430995
Bo <boburton023@gmail.com>
2022-05-18 11:40:20 +1000

    added more obstacles

--- game.py
+++ game.py
@@ -83,11 +83,76 @@ class Snake:
         if self.facing == 'down':
             self.position[1] += 1
         self.segments.insert(0, list(self.position))
+
+class Snake2:
+    def __init__(self):
+        
+        self.image_up = pygame.image.load('images/head_up.bmp')
+        self.image_down = pygame.image.load('images/head_down.bmp')
+        self.image_left = pygame.image.load('images/head_left.bmp')
+        self.image_right = pygame.image.load('images/head_right.bmp')
+
+        self.tail_up = pygame.image.load('images/tail_up.bmp')
+        self.tail_down = pygame.image.load('images/tail_down.bmp')
+        self.tail_left = pygame.image.load('images/tail_left.bmp')
+        self.tail_right = pygame.image.load('images/tail_right.bmp')
+            
+        self.image_body = pygame.image.load('images/body.bmp')
+
+        self.facing = "right"
+        self.initialize()
+
+    def initialize(self):
+        self.position = [12, 12]
+        self.segments = [[12 - i, 12] for i in range(3)]
+        self.score = 0
+
+    def blit_body(self, x, y, screen):
+        screen.blit(self.image_body, (x, y))
+        
+    def blit_head(self, x, y, screen):
+        if self.facing == "up":
+            screen.blit(self.image_up, (x, y))
+        elif self.facing == "down":
+            screen.blit(self.image_down, (x, y))  
+        elif self.facing == "left":
+            screen.blit(self.image_left, (x, y))  
+        else:
+            screen.blit(self.image_right, (x, y))  
+            
+    def blit_tail(self, x, y, screen):
+        tail_direction = [self.segments[-2][i] - self.segments[-1][i] for i in range(2)]
+        
+        if tail_direction == [0, -1]:
+            screen.blit(self.tail_up, (x, y))
+        elif tail_direction == [0, 1]:
+            screen.blit(self.tail_down, (x, y))  
+        elif tail_direction == [-1, 0]:
+            screen.blit(self.tail_left, (x, y))  
+        else:
+            screen.blit(self.tail_right, (x, y))  
+    
+    def blit(self, rect_len, screen):
+        self.blit_head(self.segments[0][0]*rect_len, self.segments[0][1]*rect_len, screen)                
+        for position in self.segments[1:-1]:
+            self.blit_body(position[0]*rect_len, position[1]*rect_len, screen)
+        self.blit_tail(self.segments[-1][0]*rect_len, self.segments[-1][1]*rect_len, screen)                
+            
+    
+    def update(self):
+        if self.facing == 'right':
+            self.position[0] += 1
+        if self.facing == 'left':
+            self.position[0] -= 1
+        if self.facing == 'up':
+            self.position[1] -= 1
+        if self.facing == 'down':
+            self.position[1] += 1
+        self.segments.insert(0, list(self.position))
         
 class Strawberry():
     def __init__(self, settings):
         self.settings = settings
-        
         self.style = str(random.randint(1, 8))
         self.image = pygame.image.load('images/food' + str(self.style) + '.png')        
         self.initialize()
@@ -105,6 +170,7 @@ class Strawberry():
         if self.position in snake.segments:
             self.random_pos(snake)
 
+
     def blit(self, screen):
         screen.blit(self.image, [p * self.settings.rect_len for p in self.position])
    
@@ -115,12 +181,12 @@ class Obstacle():
     def __init__(self, settings):
         self.settings = settings
         self.style = str(random.randint(1, 8))
-        self.image = pygame.image.load('images/obstical.png')     
+        self.image = pygame.image.load('images/rock.png')     
         self.initialize()
         
     def random_pos(self, snake):
         self.style = str(random.randint(1, 8))
-        self.image = pygame.image.load('images/obstical.png')         
+        self.image = pygame.image.load('images/rock.png')         
         
         self.position[0] = random.randint(0, self.settings.width-1)
         self.position[1] = random.randint(0, self.settings.height-1)
@@ -157,15 +223,65 @@ class rock():
     def initialize(self):
         self.position = [random.randint(0,28), random.randint(0,28)]
 
+class rock2():
+    def __init__(self, settings):
+        self.settings = settings
+        self.style = str(random.randint(1, 8))
+        self.image = pygame.image.load('images/rock.png')     
+        self.initialize()
+        
+    def random_pos(self, snake):
+        self.style = str(random.randint(1, 8))
+        self.image = pygame.image.load('images/rock.png')         
+        
+        self.position[0] = random.randint(0, self.settings.width-1)
+        self.position[1] = random.randint(0, self.settings.height-1)
+
+        self.position[0] = random.randint(9, 19)
+        self.position[1] = random.randint(9, 19)
+        
+    def blit(self, screen):
+        screen.blit(self.image, [p * self.settings.rect_len for p in self.position])
+
+    def initialize(self):
+        self.position = [random.randint(0,28), random.randint(0,28)]
+
+
+class rock3():
+    def __init__(self, settings):
+        self.settings = settings
+        self.style = str(random.randint(1, 8))
+        self.image = pygame.image.load('images/rock.png')     
+        self.initialize()
+        
+    def random_pos(self, snake):
+        self.style = str(random.randint(1, 8))
+        self.image = pygame.image.load('images/rock.png')         
+        
+        self.position[0] = random.randint(0, self.settings.width-1)
+        self.position[1] = random.randint(0, self.settings.height-1)
+
+        self.position[0] = random.randint(9, 19)
+        self.position[1] = random.randint(9, 19)
+        
+    def blit(self, screen):
+        screen.blit(self.image, [p * self.settings.rect_len for p in self.position])
+
+    def initialize(self):
+        self.position = [random.randint(0,28), random.randint(0,28)]
+
 class Game:
     """
     """
     def __init__(self):
         self.settings = Settings()
         self.snake = Snake()
+        self.snake2 = Snake2()
         self.strawberry = Strawberry(self.settings)
         self.obstacle = Obstacle(self.settings)
         self.rock = rock(self.settings)
+        self.rock2 = rock2(self.settings)
+        self.rock3 = rock3(self.settings)
         self.move_dict = {0 : 'up',
                           1 : 'down',
                           2 : 'left',
@@ -173,9 +289,12 @@ class Game:
         
     def restart_game(self):
         self.snake.initialize()
+        self.snake2.initialize()
         self.strawberry.initialize()
         self.obstacle.initialize()
         self.rock.initialize()
+        self.rock2.initialize()
+        self.rock3.initialize()
 
     def current_state(self):         
         state = np.zeros((self.settings.width+2, self.settings.height+2, 2))
@@ -184,11 +303,16 @@ class Game:
         for position in self.snake.segments:
             state[position[1], position[0], 0] = 1
         
+        for position in self.snake2.segments:
+            state[position[1], position[0], 0] = 1
+        
         state[:, :, 1] = -0.5        
 
         state[self.strawberry.position[1], self.strawberry.position[0], 1] = 0.5
         state[self.obstacle.position[1], self.obstacle.position[0], 1] = 0.5
         state[self.rock.position[1], self.rock.position[0], 1] = 0.5
+        state[self.rock2.position[1], self.rock2.position[0], 1] = 0.5
+        state[self.rock3.position[1], self.rock3.position[0], 1] = 0.5
 
         for d in expand:
             state[self.strawberry.position[1]+d[0], self.strawberry.position[0]+d[1], 1] = 0.5
@@ -196,6 +320,10 @@ class Game:
              state[self.obstacle.position[1]+d[0], self.obstacle.position[0]+d[1], 1] = 0.5
         for d in expand:
             state[self.rock.position[1]+d[0], self.rock.position[0]+d[1], 1] = 0.5
+        for d in expand:
+            state[self.rock2.position[1]+d[0], self.rock2.position[0]+d[1], 1] = 0.5
+        for d in expand:
+            state[self.rock3.position[1]+d[0], self.rock3.position[0]+d[1], 1] = 0.5
         return state
 
 
@@ -205,7 +333,7 @@ class Game:
         direction_dict = {value : key for key,value in self.move_dict.items()}
         return direction_dict[direction]
         
-    def do_move(self, move):
+    def do_move1(self, move):
         move_dict = self.move_dict
         
         change_direction = move_dict[move]
@@ -235,6 +363,26 @@ class Game:
                     
         return reward
     
+    def do_move2(self, move):
+        move_dict = self.move_dict
+        
+        change_direction1 = move_dict[move]
+        
+        if change_direction1 == 'right' and not self.snake2.facing == 'left':
+            self.snake2.facing = change_direction1
+        if change_direction1 == 'left' and not self.snake2.facing == 'right':
+            self.snake2.facing = change_direction1
+        if change_direction1 == 'up' and not self.snake2.facing == 'down':
+            self.snake2.facing = change_direction1
+        if change_direction1 == 'down' and not self.snake2.facing == 'up':
+            self.snake2.facing = change_direction1
+
+        self.snake2.update()
+                
+        if self.game_end():
+            return -1
+                    
+    
     def game_end(self):
         end = False
         if self.snake.position[0] >= self.settings.width or self.snake.position[0] < 0:
@@ -244,6 +392,10 @@ class Game:
         if self.snake.position[0] == self.obstacle.position[0] and self.snake.position[1] == self.obstacle.position[1]:
             end = True
         if self.snake.position[0] == self.rock.position[0] and self.snake.position[1] == self.rock.position[1]:
+            end = True
+        if self.snake.position[0] == self.rock2.position[0] and self.snake.position[1] == self.rock2.position[1]:
+            end = True
+        if self.snake.position[0] == self.rock3.position[0] and self.snake.position[1] == self.rock3.position[1]:
             end = True
         if self.snake.segments[0] in self.snake.segments[1:]:
             end = True
